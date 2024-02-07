7bb719eecfa9e8812f534bc11822f7ba802548cc
Roshan Patil <114917682+roshanpatil37407@users.noreply.github.com>
2024-02-07 15:10:32 +0530

    First Commit

---
+++ WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="practical_1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button runat="server" Text="Button" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace practical_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected override void OnInit(EventArgs e)
+        {
+            Response.Write("<br/> this is before initialization");
+            base.OnInit(e);
+        }
+        protected override void OnInitComplete(EventArgs e)
+        {
+            Response.Write("<br/> this is initialization");
+            base.OnInitComplete(e);
+        }
+        protected override void OnPreLoad(EventArgs e)
+        {
+            Response.Write("<br/> this is  page loading");
+            base.OnPreLoad(e);
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            Response.Write("<br/> this is before loading");
+            base.OnLoad(e);
+        }
+        protected override void OnLoadComplete(EventArgs e)
+        {
+            Response.Write("<br/> this load complete");
+            base.OnLoadComplete(e);
+        }
+        protected override void OnPreRender(EventArgs e)
+        {
+            Response.Write("<br/> this is page render ");
+            base.OnPreRender(e);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
